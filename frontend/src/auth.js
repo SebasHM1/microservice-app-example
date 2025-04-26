9adf7160370e5b1298172a73b886457e0c290dab
Sebastian Hidalgo <sebashidmar@gmail.com>
2025-04-26 15:33:40 -0500

    Hotfix: fix typo auth api address

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -7,7 +7,8 @@ import decode from 'jwt-decode'
  * @var{string} LOGIN_URL The endpoint for logging in. This endpoint should be proxied by Webpack dev server
  *    and maybe nginx in production (cleaner calls and avoids CORS issues).
  */
-const LOGIN_URL = AUTH_API_ADDRESS + '/login'
+// ✔ Usa la variable de entorno expuesta por Vue CLI
+const LOGIN_URL = process.env.VUE_APP_AUTH_API_ADDRESS + '/login'
 const ROLE_ADMIN = 'ADMIN'
 
 /**
